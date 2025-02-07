ce2a85748306db7ece8e471f8773917fa765d0b7
ljurca <lmjurca@dons.usfca.edu>
2025-02-06 17:43:09 -0800

    tests passing

--- src/front/lex.rs
+++ src/front/lex.rs
@@ -103,13 +103,36 @@ impl<'input> Lexer<'input> {
     /// - Some(token) where the token is the next token.
     /// - Some(Error) if none of the recognizers work, i.e. if there is a lexer error.
     pub fn next<'a>(&'a mut self) -> Option<Token<'input>> {
-        todo!()
+       // todo!() -> done
+       self.skip_whitespace();
+       if self.end_of_input(){
+        return None
+       }
+
+       let (kind,len) = self
+       .matchers
+       .iter()
+       .find_map(|(re, kind)|re.find(&self.input[self.pos..]).map(|m| (*kind, m.len())))
+       .unwrap_or((Error, 1));
+
+       let token = Token{
+        kind,
+        text: &self.input[self.pos..(self.pos + len)]
+       }; 
+       self.pos += len; 
+       Some(token)
     }
 }
 
 /// Read all the tokens from input
 pub fn get_tokens(input: &str) -> Vec<Token> {
-    todo!()
+   // todo!() -> done 
+    let mut lexer = Lexer::new(input);
+    let mut tokens = vec![];
+    while let Some(token) = lexer.next(){
+        tokens.push(token)
+    }
+    tokens
 }
 
 #[cfg(test)]
